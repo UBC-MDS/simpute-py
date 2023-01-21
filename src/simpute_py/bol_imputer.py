e77d79a40b7291f258ad142633c52c3cdb044a7a
LisaSeq <112457048+LisaSeq@users.noreply.github.com>
2023-01-21 14:26:28 -0800

    Created draft code for bol_impute

--- src/simpute_py/bol_imputer.py
+++ src/simpute_py/bol_imputer.py
@@ -1,15 +1,15 @@
-def bol_imputer(x,col,method):
+def bol_imputer(df,col,method='mode'): #How is the user converting their data into a df? 
     """
     Fill the empty values of a specific column with imputed values to facilitate data preprocessing.
 
     Parameters
     ----------
-    x: pandas dataframe
+    df: pandas dataframe
         A pandas dataframe waiting for imputing
     col: string
         name of the column waiting for imputing
     method: string
-        A name of the imputing strategy. They include ['knn','frequent','bayes', 'rand'], the default method is knn.
+        A name of the imputing strategy. They include ['mode','rand'], the default method is method.
     Returns
     ----------
     y: pandas dataframe
@@ -20,4 +20,43 @@ def bol_imputer(x,col,method):
     >>> from simpute import bol_imputer
     >>> bol_imputer(row_df,'if_default_last_month','knn')
     """
+    import pandas as pd
+    import numpy as np
+    
+    bool_col = df[col]
+
+    #Replace any empty fields with nan
+    bool_col.replace('', np.nan, inplace=True)
+
+    #Check if the column does have null values
+    if bool_col.isnull().sum() == 0:
+        return print("Your column does not have any missing values to impute.")
+
+    #Check if the column is boolean
+    if (bool_col.dropna().isin([True, False, 1, 0, 'True', 'False'])).all() == False:
+        return print("You column is not Boolean, please select another column.")
+
+    #Calculate frequencies of boolean values
+    count_true = 0
+    count_false = 0
+
+    for bool in bool_col.dropna():
+        
+        if bool in [True, 'True', 1]:
+            count_true += 1
+        else:
+            count_false += 1
+    
+    freq_true = count_true/len(bool_col.dropna())
+    freq_false = count_false/len(bool_col.dropna())
+
+    if freq_true == freq_false or freq_true > freq_false:
+        impute_value = True 
+    else:
+        impute_value = False
+    
+    for i in bool_col:
+        if df[col]
+
+
     return
