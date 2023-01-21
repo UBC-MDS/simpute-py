628226f5eaf98cf7f28d7dc238fcd4123ba42554
Ken Wang <kenuiuc@gmail.com>
2023-01-21 14:45:54 -0800

    add pandas to toml

--- src/simpute_py/date_imputer.py
+++ src/simpute_py/date_imputer.py
@@ -1,2 +1,3 @@
-def date_imputer(data, col):
-    return "hello"
+def date_imputer(input_df, col):
+    input_df.loc[:,col] = input_df.loc[:,col].replace({None: datetime.today()})
+    return input_df
