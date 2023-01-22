554d96ae64da6678292dd51f07237072a95e0fc0
LisaSeq <112457048+LisaSeq@users.noreply.github.com>
2023-01-21 17:16:21 -0800

    Updated the assert statement in test_bool_simpute_py

--- tests/test_bool_simpute_py.py
+++ tests/test_bool_simpute_py.py
@@ -3,28 +3,23 @@ from bol_imputer import bol_imputer
 import pandas as pd
 import numpy as np
 
-def test_impute_freq():
+def test_bool_impute():
     #Test imputing is working as expected
     test_df = pd.DataFrame({'bool_1': [True, False, False, "", "", "True", "False", 1, 0], 'bool_2':["","","","","",0,1,0,1]})
     bol_imputer(test_df, 'bool_1')
     solution_df_1 = pd.DataFrame({'bool_1': [True, False, False, False, False, "True", "False", 1, 0], 'bool_2':["","","","","",0,1,0,1]})
     assert test_df.equals(solution_df_1) == True, "Imputer is not working."
 
-    #Test imputing on edge case of the same frequencies
+    #Testing imputer only changed column indicated
+    assert solution_df_1['bool_2'].equals(test_df['bool_2']) == True, "Imputer is changing columns other than the one selected for imputation."
+
+    #Testing edge case of the same frequencies of 'True' and 'False'
     bol_imputer(test_df, 'bool_2')
     solution_df_2 = pd.DataFrame({'bool_1': [True, False, False, False, False, "True", "False", 1, 0], 'bool_2':[True,True,True,True,True,0,1,0,1]})
-    assert test_df.equals(solution_df_2) == True, "Imputer is not working."
+    assert test_df.equals(solution_df_2) == True, "Imputer is not imputing 'True' when boolean frequencies are the same."
 
-    #Test for to check for any null values
-    assert test_df.isna().any().any() == False, "Imputer did not impute all null values"
+    #Test to check for any null values in the output
+    assert test_df.isna().any().any() == False, "Imputer did not impute all null values of boolean columns"
 
-def test_exception_cases():
-    """Test cases where input is incorrect"""
-    test_df = pd.DataFrame({'bool_1': [True, False, False, "", "", "True", "False", 1, 0]})
-
-    with pytest.raises(Exception):
-        bol_imputer(df=None, col='bool_1')
-    with pytest.raises(Exception):
-        bol_imputer(df=test_df, col=None)
-    with pytest.raises(Exception):
-        bol_imputer(df=test_df, col='Non_existant_column')
+    #Testing that the output is a dataframe
+    assert isinstance(test_df, pd.DataFrame), "Imputer did not output a dataframe"
