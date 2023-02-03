f691cad3dc6982a75ff917a4a9d6a7a2f4b032c9
Ken Wang <kenuiuc@gmail.com>
2023-02-03 13:10:20 -0800

    feat: median date imputer implementation

--- src/simpute_py/date_imputer.py
+++ src/simpute_py/date_imputer.py
@@ -1,6 +1,39 @@
+import pandas
 import datetime
+import time
+
+
+def date_to_timestamp(date_datetime):
+    """ Convert a date into an epoch timestamp value
+
+    Args:
+        date_datetime (datetime.date): date value
+
+    Returns:
+        float: unix epoch timestamp value
+    """
+    if pandas.isnull(date_datetime):
+        return None
+    elif isinstance(date_datetime, datetime.date):
+        return time.mktime(date_datetime.timetuple())
+    else:
+        return None
 
 def date_imputer(input_df, col):
-    today = datetime.date.today()
-    input_df[col] = input_df[col].replace({None: today})
+    """ Imputation method for date type column
+
+    Args:
+        input_df (Pandas.DataFrame): input dataframe
+        col (string): Name of the date column that needs imputation
+
+    Returns:
+        Pandas.DateFrame: dateframe after imputation of the dedicated column
+    """
+    date_series = input_df[col]
+    epoch_timestamp_series = date_series.apply(date_to_timestamp)
+    min_epoch_timestamp = epoch_timestamp_series.min()
+    max_epoch_timestamp = epoch_timestamp_series.max()
+    median_epoch_timestamp = (min_epoch_timestamp + max_epoch_timestamp)/2
+    median_date = datetime.date.fromtimestamp(median_epoch_timestamp)
+    input_df[col] = input_df[col].replace({None: median_date})
     return input_df
